a451cac78366e047d2c6cd02ace1cfaf2bbfdb88
SaiVarun107 <saivarunbitra107@gmail.com>
2025-05-26 22:24:00 +0530

    Delete src directory

--- src/main/java/com/example/addressbook/exception/AddressBookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.addressbook.exception;
-
-public class AddressBookNotFoundException extends RuntimeException {
-    public AddressBookNotFoundException(Long id) {
-        super("Address book entry not found with id: " + id);
-    }
-}
--- src/main/java/com/example/addressbook/repository/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.addressbook.repository;
-
-import com.example.addressbook.model.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-}
